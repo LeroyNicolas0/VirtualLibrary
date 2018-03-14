f2be8fefba256bfa46fd052df781dd89d120bd84
LeroyNicolas0 <atylan1@gmail.com>
2018-03-14 15:14:24 -0400

    Add some java files

---
+++ VirtualLibraryWeb/src/java/Beans/MotCle.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+/**
+ *
+ * @author atyla
+ */
+public class MotCle {
+    protected Integer motCleID;
+
+    public Integer getMotCleID() {
+        return motCleID;
+    }
+
+    public void setMotCleID(Integer motCleID) {
+        this.motCleID = motCleID;
+    }
+    private String nom;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+}
---
+++ VirtualLibraryWeb/src/java/Beans/These.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+/**
+ *
+ * @author atyla
+ */
+public class These extends Document{
+    private Boolean validation;
+
+    public Boolean getValidation() {
+        return validation;
+    }
+
+    public void setValidation(Boolean validation) {
+        this.validation = validation;
+    }
+}
