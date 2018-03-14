4546602de3adb18dd3742413f929adb75c92995b
KHAN <fury95@live.fr>
2018-03-14 12:46:43 -0400

    debut filter

---
+++ VirtualLibraryWeb/src/java/Beans/Participant.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Beans;
+
+/**
+ *
+ * @author nicolas
+ */
+public class Participant {
+    private String nom;
+    private String prenom;
+    private String pseudonyme;
+    private int typeParticipant;
+}
